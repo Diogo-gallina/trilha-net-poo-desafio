0caab3e53f47cf336d7d31b6932c54c1ce0caf70
Diogo-gallina <diogogallina20@gmail.com>
2022-09-30 20:18:07 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("(11)92343-6786", "Nokia g50", "123456", 128); 
+nokia.Ligar();
+nokia.InstalarAplicativo("Google Maps");
+
+Console.WriteLine("\n-------------------------------------------------------------------------\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("(11)95493-4278", "Iphone g50", "654321", 256);
+iphone.ReceberLigacao(); 
+iphone.InstalarAplicativo("Waze");
